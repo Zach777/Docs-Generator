9aa9b5232b3b410e7095e88caf1bfcbcd9ac7dc8
Zach777 <MeteorCity3109@protonmail.com>
2020-05-20 21:41:49 -0400

    Check that / or \ is present in text input.

--- SceneComponent/FolderPathHandler/FolderPathHandler.gd
+++ SceneComponent/FolderPathHandler/FolderPathHandler.gd
@@ -1,9 +1,31 @@
-extends HBoxContainer
+extends VBoxContainer
 
-onready var path_holder : LineEdit = $PathToFolder
+onready var path_holder : LineEdit = $InputField/PathToFolder
 
 #Emitted when the user has confirmed what path to use.
 signal path_confirmed(path_to_folder_string)
 
+#Emitted when there is not a slash at the end of the filename.
+signal path_entered_wrong()
+
+
+#Check that the player inputted slashes.
+#warning-ignore:unused_argument
+func _process(delta : float) -> void :
+	#Return if there is no text inputted.
+	if path_holder.text == "" :
+		$PathIncorrectMessage.hide()
+		return
+	
+	if path_holder.text.ends_with("/") == false && path_holder.text.ends_with("\\") == false :
+		$PathIncorrectMessage.show()
+	
+	else :
+		$PathIncorrectMessage.hide()
+
 func _on_Confirm_pressed():
-	emit_signal("path_confirmed", path_holder.text)
+	if path_holder.text.ends_with("/") || path_holder.text.ends_with("\\") :
+		emit_signal("path_confirmed", path_holder.text)
+	
+	else :
+		emit_signal("path_entered_wrong")
