e6cb18692b4f45a2349d44d07be4e9c0f5d94aa8
Zach777 <MeteorCity3109@protonmail.com>
2020-06-17 08:26:27 -0400

    Contain all files in res://addons/DocsGenerator

--- addons/DocsGenerator/helpers/Manager.gd
+++ addons/DocsGenerator/helpers/Manager.gd
@@ -7,6 +7,6 @@ func _path_confirmed(path_to_folder_string : String) -> void:
 	var file_array : Array
 	file_array = $RecursiveGDFileGetter.list_all_gd_files_from_directory_relative(path_to_folder_string)
 	for gd_file in file_array :
-		$DocLoader.generate_doc_from_gd(path_to_folder_string + gd_file)
+		$GDFileParser.generate_doc_from_gd(path_to_folder_string + gd_file)
 	
-	$DocLoader.close_save_file()
+	$GDFileParser.close_save_file()
